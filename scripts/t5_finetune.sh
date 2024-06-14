8d1a54abd63561b0fd17893c878ba634aa9aa2dd
BenKabongo25 <kabongo.ben025@gmail.com>
2024-06-14 15:04:08 +0200

    scripts

--- scripts/t5_finetune.sh
+++ scripts/t5_finetune.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
-#SBATCH --partition=funky
-#SBATCH --job-name=reco_nlp
+#SBATCH --partition=electronic
+#SBATCH --job-name=p5_small
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --time=6000
-#SBATCH --output=reco_nlp_all_beauty_ratings.out
-#SBATCH --error=reco_nlp_all_beauty_ratings.err
+#SBATCH --output=t5_finetune_beauty.out
+#SBATCH --error=t5_finetune_beauty.err
 
-python /home/kabongo/reco_nlp/reco_nlp/llms_baseline/prompting.py \
+python /home/kabongo/reco_nlp/reco_nlp/llms_baseline/t5_finetuning.py \
+    --model_name_or_path google/flan-t5-small \
+    --tokenizer_name_or_path google/flan-t5-small \
+    --max_source_length 1024 \
+    --max_target_length 8 \
     --base_dir /home/kabongo/reco_nlp/data/process \
     --dataset_name All_Beauty \
-    --lang en \
-    --verbose \
+    --train_dataset_path /home/kabongo/reco_nlp/data/process/All_Beauty/samples/dir/train/sampled_data.csv \
+    --test_dataset_path /home/kabongo/reco_nlp/data/process/All_Beauty/samples/dir/test/sampled_data.csv \
+    --exp_name t5_small_rating \
+    --train_size 0.8 \
+    --n_epochs 100 \
     --batch_size 16 \
-    --evaluate_every 10 \
+    --lr 0.001 \
+    --save_every 10 \
+    --max_review_length 128 \
+    --max_description_length 128 \
+    --min_rating 1.0 \
+    --max_rating 5.0 \
+    --no-user_description_flag \
+    --item_description_flag \
     --base_data_size 0.25 \
     --max_base_data_samples 10000 \
     --split_method 0 \
@@ -41,7 +55,4 @@ python /home/kabongo/reco_nlp/reco_nlp/llms_baseline/prompting.py \
     --user_first_flag \
     --no-target_review_flag \
     --target_rating_flag \
-    --max_source_length 1024 \
-    --max_target_length 128 \
-    --model_name_or_path google/flan-t5-base \
-    --tokenizer_name_or_path google/flan-t5-base \
+    
